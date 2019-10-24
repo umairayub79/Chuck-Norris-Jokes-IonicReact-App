c756ff91df656ef7cfc608a05e841bfc979e8a19
umairayub79 <umairayub79@gmail.com>
2019-10-24 20:49:04 +0500

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,50 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItem } from '@ionic/react';
+import {FC, useState, useEffect } from 'react';
+import axios from 'axios';
 import React from 'react';
 
-const Home: React.FC = () => {
+
+const URL = `http://api.icndb.com/jokes/random/30`;
+
+// fetch all articles
+const fetchJokes = () => {
+  return axios({
+    url: URL,
+    method: 'get'
+  }).then(response => {
+    console.log(response);
+    return response.data;
+  })
+};
+
+const Home: FC = () => {
+
+  const [ jokes, setJokes ] = useState([]);
+  // const items: any[] = [];
+
+  useEffect(() => {
+    fetchJokes().then(data => setJokes(data.value));
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Chuck Norris Jokes</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <IonList>
+          {
+            jokes.map((a, index) => {
+              return(
+                <IonItem key={index}>
+                  {a['joke']}
+                </IonItem>
+              )
+            })
+          }
+        </IonList>
       </IonContent>
     </IonPage>
   );
